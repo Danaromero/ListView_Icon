79c9b8e97d1f2e3e3a6953fd3d10dbb07a7734b3
FlutLab.io <>
2021-05-24 17:06:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class DanaApp extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 10.0),
             ),
             ListTile(
-              leading: Icon(Icons.masks,  color: Colors.purple),
+              leading: Icon(Icons.masks,  color: Colors.indigo),
               title: Text(
                 'Cubre Bocas',
                 textScaleFactor: 1.5,
