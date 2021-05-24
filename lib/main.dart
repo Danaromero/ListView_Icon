7f5333ceb4395bdab56ef87b89d5079b2cf87908
FlutLab.io <>
2021-05-24 16:11:42 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class DanaApp extends StatelessWidget {
           centerTitle: true,
           title: Text(title, style: TextStyle(
           color: Colors.black ),),
-          backgroundColor: Colors.pinkAccent[100],
+          backgroundColor: Colors.pinkAccent[50],
         ),
         body: Center(
         child: ListView(
